31b6b96d238b94ebace2905ce801882693f0fe48
madisonsilver <sir.seldom.vain@gmail.com>
2017-08-27 06:34:51 -0700

    Update lambda.cpp

--- lambda.cpp
+++ lambda.cpp
@@ -61,14 +61,31 @@ OUT func<IN, OUT>::apply(IN i){
 template <class IN, class OUT>
 class lambda{
     func<IN,OUT>* pntr;
-
+    public:
     OUT apply(IN i){
         return pntr->apply(i);
     };
-    
+    void erase (){
+        func<IN,OUT>::erase(pntr);
+    };
+    void copy (lambda x){
+        pntr=func<IN,OUT>::copy(x.pntr);
+    };
+    void lapply(OUT (*mod)(IN, func<IN,OUT>*)){
+        pntr = func<IN,OUT>::apply(mod,pntr);
+    };
+    void setPntr(func<IN,OUT>* f){
+        pntr=f;
+    };
+    void setFunc(OUT (*f)(IN)){
+        pntr=func<IN,OUT>::convert(f);
+    };
 };
 
 
+
+
+//Examples
 int doubler(int i, func<int,int>* f){
     return 2*f->apply(2*i);
 }
@@ -82,8 +99,11 @@ int a(int x){
 
 int main(){
     func<int,int>* da = func<int,int>::apply(doubler, func<int,int>::convert(a));
+    lambda<int,int> good;
+    good.setFunc(a);
+    good.lapply(doubler);
     for (int i =0; i<11; i++){
-        printf("%i\t%i\n",i,da->apply(i));
+        printf("%i\t%i\n",i,good.apply(i));
     }
     func<int,int>::erase(da);
 }
